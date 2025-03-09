18f59c098ac2d71b6008e5993e11e86a08135f08
ArseneMJ <198416097+ArseneMJ@users.noreply.github.com>
2025-03-09 08:36:40 +0300

    Update server.py

--- server.py
+++ server.py
@@ -12,7 +12,7 @@ async def detect_emotion() :
     try:
         text_to_analyze = request.args.get('textToAnalyze')
         if(text_to_analyze ==''):
-            return ' Invalid text! Please try again!',40
+            return ' Invalid text! Please try again!',400
         emotion_data = await emotion_detector(text_to_analyze)
         respond = f"For the given statement, the system response is 'anger': {emotion_data['anger']}, 'disgust': {emotion_data['disgust']}, 'fear': {emotion_data['fear']}, 'joy': {emotion_data['joy']}, 'sadness': {emotion_data['sadness']}. The dominant emotion is {emotion_data['dominant_emotion']}."
         return respond,200
